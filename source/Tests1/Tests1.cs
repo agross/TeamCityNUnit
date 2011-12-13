fbd0387372740a4a284aa1879da5bddd72917cfe
Alexander Groß <agross@therightstuff.de>
2011-12-13 17:13:42 +0100

    Implementing some useless tests

---
+++ source/Tests1/Tests1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Tests1
+{
+  [TestFixture]
+  public class Tests1
+  {
+    [Test]
+    public void TheFirstTest()
+    {
+    }
+
+    [Test]
+    public void TheSecondTest()
+    {
+    }
+
+    [Test]
+    public void TheThirdTest()
+    {
+    }
+  }
+}
---
+++ source/Tests2/Tests2.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Tests2
+{
+  [TestFixture]
+  public class Tests2
+  {
+    [Test]
+    public void TheFirstTest()
+    {
+    }
+
+    [Test]
+    public void TheSecondTest()
+    {
+    }
+
+    [Test]
+    public void TheThirdTest()
+    {
+    }
+  }
+}
