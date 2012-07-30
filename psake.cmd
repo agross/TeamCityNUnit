e1fd9277cb27792c4e6c287226813fc986e81fe4
Alexander Groß <agross@therightstuff.de>
2012-07-30 12:05:34 +0200

    Honor psake's exit code when using psake.cmd

--- psake.cmd
+++ psake.cmd
@@ -1,1 +1,13 @@
-@powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& { .\packages\psake.4.0.1.0\tools\psake.ps1 %* }"
+@echo off
+
+set psake=packages\psake.4.0.1.0\tools\psake.ps1
+
+if '%1'=='/?' goto help
+if '%1'=='-help' goto help
+if '%1'=='-h' goto help
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%psake%' %*; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }"
+goto :eof
+
+:help
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%psake%' -help"
